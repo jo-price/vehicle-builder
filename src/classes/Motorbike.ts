519432b79930c8f6f0a161854ced09b6294fa76b
Spicy Moo Deng <188100478+jo-price@users.noreply.github.com>
2025-02-16 16:15:24 -0600

    fix motorbike and wheelie

--- src/classes/Motorbike.ts
+++ src/classes/Motorbike.ts
@@ -39,7 +39,7 @@ constructor(
   this.year = year;
   this.weight = weight;
   this.topSpeed = topSpeed;
-  if (wheels.length !== 4) {
+  if (wheels.length !== 2) {
     this.wheels = [new Wheel(), new Wheel()];
   } else {
     this.wheels = wheels;
@@ -47,26 +47,26 @@ constructor(
 }
   // TODO: Implement the wheelie method
     // TODO: The method should log the message "Motorbike [make] [model] is doing a wheelie!"
-  wheelie(this.vehicles); {
+  wheelie():void {
   console.log(`Motorbike ${this.make} ${this.model} is doing a wheelie!`);
     }
-  }
+  
   // TODO: Override the printDetails method from the Vehicle class
   // TODO: The method should call the printDetails method of the parent class
   // TODO: The method should log the details of the Motorbike
   // TODO: The details should include the VIN, make, model, year, weight, top speed, color, and wheels
   override printDetails(): void {
-    super.printDetails(),
+    super.printDetails();
 
-    console.log(`VIN: ${this.vin}`),
-    console.log(`Make: ${this.make}`),
-    console.log(`Model: ${this.model}`),
-    console.log(`Year: ${this.year}`),
-    console.log(`Weight: ${this.weight} lbs`),
-    console.log(`Top Speed: ${this.topSpeed} mph`),
-    console.log(`Color: ${this.color}`),
-    console.log(`Wheel 1: ${this.wheels[0].getDiameter} inch with a ${this.wheels[0].getTireBrand} tire`),
-    console.log(`Wheel 2: ${this.wheels[1].getDiameter} inch with a ${this.wheels[1].getTireBrand} tire`),
-  }
+    console.log(`VIN: ${this.vin}`);
+    console.log(`Make: ${this.make}`);
+    console.log(`Model: ${this.model}`);
+    console.log(`Year: ${this.year}`);
+    console.log(`Weight: ${this.weight} lbs`);
+    console.log(`Top Speed: ${this.topSpeed} mph`);
+    console.log(`Color: ${this.color}`);
+    console.log(`Wheel 1: ${this.wheels[0].getDiameter} inch with a ${this.wheels[0].getTireBrand} tire`);
+    console.log(`Wheel 2: ${this.wheels[1].getDiameter} inch with a ${this.wheels[1].getTireBrand} tire`);
+  }}
 // Export the Motorbike class as the default export
 export default Motorbike;
